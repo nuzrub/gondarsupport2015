cbf6eb5ec36586cde5e78e12451969f1953a207d
Ygor Rebouças <ygor.reboucas@gmail.com>
2015-01-23 20:33:21 -0300

    Finalizado Input. Feito a câmera e um player básico.

---
+++ Assets/Scripts/Debug Player/BasicBullet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicBullet : MonoBehaviour {
+
+	public Vector3 Speed;
+
+	void Update () {
+		transform.Translate (Speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float Speed = 60;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		Mover ();
+	}
+
+	private void Mover() {
+		Vector3 movement = GameInput.MoveDirection ();
+
+		transform.Translate (movement * Speed * Time.deltaTime);
+	}
+}
