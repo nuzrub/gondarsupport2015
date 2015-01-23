2d2043ccf4186e41b8ab717f20c93e12ddd67339
Victor <victorcdcc@gmail.com>
2015-01-23 20:31:48 -0300

    Corpo do Monstro.

---
+++ Assets/Scripts/Inimigos/CorpoMonstro.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorpoMonstro : MonoBehaviour {
+	
+	public Transform slotCabeca;
+	public Transform slotBracoEsquerdo;
+	public Transform slotBracoDireito;
+	public Transform slotCosta;
+	public Transform slotPernas;
+
+	void criarMonstro(Transform cabeca, Transform bracoEsquerdo, Transform bracoDireito, Transform costa, Transform pernas){
+		cabeca.parent = slotCabeca;
+		cabeca.localPosition = Vector3.zero;
+		bracoDireito.parent = slotBracoDireito;
+		bracoDireito.localPosition = Vector3.zero;
+		bracoEsquerdo.parent = slotBracoEsquerdo;
+		bracoEsquerdo.localPosition = Vector3.zero;
+		costa.parent = slotCosta;
+		costa.localPosition = Vector3.zero;
+		pernas.parent = slotPernas;
+		pernas.localPosition = Vector3.zero;
+	}
+	
+}
