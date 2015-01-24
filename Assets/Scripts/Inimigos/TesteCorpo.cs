80af18a21e1fc1f06a1db97bc39d48892d19673a
Victor <victorcdcc@gmail.com>
2015-01-24 12:50:59 -0300

    IA inimigo.

--- Assets/Scripts/Inimigos/TesteCorpo.cs
+++ Assets/Scripts/Inimigos/TesteCorpo.cs
@@ -8,7 +8,7 @@ public class TesteCorpo : MonoBehaviour {
 	public GameObject go;
 
 	void Start () {
-		GameObject g1 = (GameObject)GameObject.Instantiate (prefab);
+		//GameObject g1 = (GameObject)GameObject.Instantiate (prefab);
 		GameObject g2 = (GameObject)GameObject.Instantiate (prefab);
 		GameObject g3 = (GameObject)GameObject.Instantiate (prefab);
 		GameObject g4 = (GameObject)GameObject.Instantiate (prefab);
---
+++ Assets/Scripts/MovimentacaoInimigo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimentacaoInimigo : MonoBehaviour {
+
+	public Transform player;
+	public int moveSpeed = 4;
+	public int MaxDist = 10;
+	public int MinDist = 5;
+	
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update () 
+	{
+		transform.LookAt(player);
+		
+		if(Vector3.Distance(transform.position,player.position) >= MinDist){
+			
+			transform.position += transform.forward*moveSpeed*Time.deltaTime;
+
+			
+			if(Vector3.Distance(transform.position,player.position) <= MaxDist)
+			{
+				//Codigo para atirar.
+			} 
+			
+		}
+	}
+}
