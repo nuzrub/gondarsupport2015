478ab2f8e858b7e00d9d19fe5b39c54d536aaa9b
Ygor Rebouças <ygor.reboucas@gmail.com>
2015-01-24 13:00:35 -0300

    Colocando colisão nos inimigos

--- Assets/Scripts/MovimentacaoInimigo.cs
+++ Assets/Scripts/MovimentacaoInimigo.cs
@@ -3,30 +3,30 @@ using System.Collections;
 
 public class MovimentacaoInimigo : MonoBehaviour {
 
-	public Transform player;
-	public int moveSpeed = 4;
-	public int MaxDist = 10;
-	public int MinDist = 5;
-	
-	void Start () 
-	{
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-	}
-	
-	void Update () 
-	{
-		transform.LookAt(player);
-		
-		if(Vector3.Distance(transform.position,player.position) >= MinDist){
-			
-			transform.position += transform.forward*moveSpeed*Time.deltaTime;
+    public Transform player;
+    public int moveSpeed = 4;
+    public int MaxDist = 10;
+    public int MinDist = 5;
 
-			
-			if(Vector3.Distance(transform.position,player.position) <= MaxDist)
-			{
-				//Codigo para atirar.
-			} 
-			
-		}
-	}
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update() {
+        transform.LookAt(player);
+
+        if (Vector3.Distance(transform.position, player.position) >= MinDist) {
+
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+
+            if (Vector3.Distance(transform.position, player.position) <= MaxDist) {
+                //Codigo para atirar.
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        rigidbody.velocity = Vector3.zero;
+    }
 }
