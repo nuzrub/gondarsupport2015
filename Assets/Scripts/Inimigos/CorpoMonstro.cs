ab60d8f9296b695ca45d2f1fc2a5f4645bcb7e07
Ygor Rebouças <ygor.reboucas@gmail.com>
2015-01-23 23:47:47 -0300

    Fazendo algumas mudanças de reorganização do projeto.

--- Assets/Scripts/Inimigos/CorpoMonstro.cs
+++ Assets/Scripts/Inimigos/CorpoMonstro.cs
@@ -9,17 +9,24 @@ public class CorpoMonstro : MonoBehaviour {
 	public Transform slotCosta;
 	public Transform slotPernas;
 
+    public float Speed;
+    public float HP_Max;
+    public float HP_Current;
+
 	void criarMonstro(Transform cabeca, Transform bracoEsquerdo, Transform bracoDireito, Transform costa, Transform pernas){
 		cabeca.parent = slotCabeca;
 		cabeca.localPosition = Vector3.zero;
+
 		bracoDireito.parent = slotBracoDireito;
 		bracoDireito.localPosition = Vector3.zero;
+
 		bracoEsquerdo.parent = slotBracoEsquerdo;
 		bracoEsquerdo.localPosition = Vector3.zero;
+
 		costa.parent = slotCosta;
 		costa.localPosition = Vector3.zero;
+
 		pernas.parent = slotPernas;
 		pernas.localPosition = Vector3.zero;
 	}
-	
 }
