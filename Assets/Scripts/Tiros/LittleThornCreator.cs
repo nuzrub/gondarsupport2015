6aaeea6542dd435cd0d9a2aa1563b4fdcb2a162b
Ygor Rebouças <ygor.reboucas@gmail.com>
2015-01-25 17:19:40 -0300

    tiro de espinhos

---
+++ Assets/Scripts/Tiros/LittleThornCreator.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LittleThornCreator : BaseShooter
+{
+
+    public float timerDestroySphere;
+    public GameObject efeito;
+    public GameObject littleThorn;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timerDestroySphere -= Time.deltaTime;
+
+        if (timerDestroySphere < 0)
+        {
+            Destroy(this.gameObject);
+            CMin.atual--;
+
+            GameObject little1 = Instantiate(littleThorn, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+            GameObject little2 = Instantiate(littleThorn, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+            GameObject little3 = Instantiate(littleThorn, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+            GameObject little4 = Instantiate(littleThorn, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+
+            Rigidbody rigidbody1 = little1.AddComponent<Rigidbody>();
+            Rigidbody rigidbody2 = little2.AddComponent<Rigidbody>();
+            Rigidbody rigidbody3 = little3.AddComponent<Rigidbody>();
+            Rigidbody rigidbody4 = little4.AddComponent<Rigidbody>();
+
+            rigidbody1.useGravity = false;
+            rigidbody1.velocity = this.gameObject.transform.forward * 30.0f;
+
+            rigidbody2.useGravity = false;
+            rigidbody2.velocity = -this.gameObject.transform.forward * 30.0f;
+
+            rigidbody3.useGravity = false;
+            rigidbody3.velocity = this.gameObject.transform.right * 30.0f;
+
+            rigidbody4.useGravity = false;
+            rigidbody4.velocity = -this.gameObject.transform.right * 30.0f;
+
+            CriarEfeito(little1.transform);
+            CriarEfeito(little2.transform);
+            CriarEfeito(little3.transform);
+            CriarEfeito(little4.transform);
+
+            Destroy(little1, 0.6f);
+            Destroy(little2, 0.6f);
+            Destroy(little3, 0.6f);
+            Destroy(little4, 0.6f);
+
+        }
+	}
+}
--- Assets/Scripts/Tiros/ThornSphereShooter.cs
+++ Assets/Scripts/Tiros/ThornSphereShooter.cs
@@ -7,62 +7,62 @@ public class ThornSphereShooter : BaseShooter {
         base.Atirar();
 
         GameObject sphere1 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
-        GameObject sphere2 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
-        GameObject sphere3 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
-        GameObject sphere4 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
-        GameObject sphere5 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
+        //GameObject sphere2 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
+        //GameObject sphere3 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
+        //GameObject sphere4 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
+        //GameObject sphere5 = Instantiate(projectile, new Vector3(Random.Range(-3.0F, 5.0F), empty.transform.position.y, Random.Range(-3.0F, 5.0F)), Quaternion.identity) as GameObject;
 
         //sphere.renderer.enabled = false;
 
         sphere1.transform.position = empty.transform.position;
         sphere1.transform.rotation = empty.transform.rotation;
 
-        sphere2.transform.position = empty.transform.position;
-        sphere2.transform.rotation = empty.transform.rotation;
+        //sphere2.transform.position = empty.transform.position;
+        //sphere2.transform.rotation = empty.transform.rotation;
 
-        sphere3.transform.position = empty.transform.position;
-        sphere3.transform.rotation = empty.transform.rotation;
+        //sphere3.transform.position = empty.transform.position;
+        //sphere3.transform.rotation = empty.transform.rotation;
 
-        sphere4.transform.position = empty.transform.position;
-        sphere4.transform.rotation = empty.transform.rotation;
+        //sphere4.transform.position = empty.transform.position;
+        //sphere4.transform.rotation = empty.transform.rotation;
 
-        sphere5.transform.position = empty.transform.position;
-        sphere5.transform.rotation = empty.transform.rotation;
+        //sphere5.transform.position = empty.transform.position;
+        //sphere5.transform.rotation = empty.transform.rotation;
 
 
         Rigidbody rigidbody1 = sphere1.AddComponent<Rigidbody>();
-        Rigidbody rigidbody2 = sphere2.AddComponent<Rigidbody>();
-        Rigidbody rigidbody3 = sphere3.AddComponent<Rigidbody>();
-        Rigidbody rigidbody4 = sphere4.AddComponent<Rigidbody>();
-        Rigidbody rigidbody5 = sphere5.AddComponent<Rigidbody>();
+        //Rigidbody rigidbody2 = sphere2.AddComponent<Rigidbody>();
+        //Rigidbody rigidbody3 = sphere3.AddComponent<Rigidbody>();
+        //Rigidbody rigidbody4 = sphere4.AddComponent<Rigidbody>();
+        //Rigidbody rigidbody5 = sphere5.AddComponent<Rigidbody>();
 
         rigidbody1.useGravity = false;
-        rigidbody1.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
-        rigidbody2.useGravity = false;
-        rigidbody2.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
-        rigidbody3.useGravity = false;
-        rigidbody3.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
-        rigidbody4.useGravity = false;
-        rigidbody4.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
-        rigidbody5.useGravity = false;
-        rigidbody5.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
+        rigidbody1.velocity = empty.transform.forward * (speed + 2);
+        //rigidbody2.useGravity = false;
+        //rigidbody2.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
+        //rigidbody3.useGravity = false;
+        //rigidbody3.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
+        //rigidbody4.useGravity = false;
+        //rigidbody4.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
+        //rigidbody5.useGravity = false;
+        //rigidbody5.velocity = empty.transform.forward * (speed + Random.Range(10.0F, 30F));
 
         CriarEfeito(sphere1.transform);
-        CriarEfeito(sphere2.transform);
-        CriarEfeito(sphere3.transform);
-        CriarEfeito(sphere4.transform);
-        CriarEfeito(sphere5.transform);
+        //CriarEfeito(sphere2.transform);
+        //CriarEfeito(sphere3.transform);
+        //CriarEfeito(sphere4.transform);
+        //CriarEfeito(sphere5.transform);
 
         sphere1.tag = "playerbullet";
-        sphere2.tag = "playerbullet";
-        sphere3.tag = "playerbullet";
-        sphere4.tag = "playerbullet";
-        sphere5.tag = "playerbullet";
-
+        //sphere2.tag = "playerbullet";
+        //sphere3.tag = "playerbullet";
+        //sphere4.tag = "playerbullet";
+        //sphere5.tag = "playerbullet";
+        
         Destroy(sphere1, timeToDestroy);
-        Destroy(sphere2, timeToDestroy);
-        Destroy(sphere3, timeToDestroy);
-        Destroy(sphere4, timeToDestroy);
-        Destroy(sphere5, timeToDestroy);
+        //Destroy(sphere2, timeToDestroy);
+        //Destroy(sphere3, timeToDestroy);
+        //Destroy(sphere4, timeToDestroy);
+        //Destroy(sphere5, timeToDestroy);
     }
 }
