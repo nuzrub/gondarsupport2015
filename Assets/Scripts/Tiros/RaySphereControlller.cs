f1c83a9c922d1acfb26243eb872c96ea257665df
Wilmerson da Silva <wilmersondasilva@gmail.com>
2015-01-25 12:21:15 -0200

    Wind alterado

---
+++ Assets/Scripts/Tiros/RaySphereControlller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaySphereControlller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag.Equals("Respawn"))
+		{
+			Debug.Log ("Bateu no inimigo");
+		}
+
+	}
+}
