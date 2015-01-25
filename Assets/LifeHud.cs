c2c808ec1aeb3726b9f8b8ec884eb5d9b90a4490
Edson Gustavo Santiago Silva <EdsonGustavo@EdsonGustavo.local>
2015-01-25 12:21:36 -0300

    hud animation method done

---
+++ Assets/LifeHud.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeHud : MonoBehaviour {
+
+	Animation animation;
+
+	// Use this for initialization
+	void Start () {
+	
+		animation = GetComponent<Animation> ();	
+	
+	}
+	
+	public void StartAnimation() {
+		
+		animation.Play ();
+
+	}
+
+}
